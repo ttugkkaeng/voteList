fb61a0fcf3d86a686352f0f22b78d8a39d1224b6
chocosong2 <greentea030@naver.com>
2022-09-28 15:38:00 +0900

    VoteResultDTO

---
+++ voteList/src/main/java/voteList/VoteResultDTO.java
@@ -0,0 +1,16 @@
+package voteList;
+
+public class VoteResultDTO {
+	private String selectNum;
+
+	public String getSelectNum() {
+		return selectNum;
+	}
+
+	public void setSelectNum(String selectNum) {
+		this.selectNum = selectNum;
+	}
+	
+	
+	
+}
--- voteList/src/main/java/voteList/voteController.java
+++ voteList/src/main/java/voteList/voteController.java
@@ -7,53 +7,20 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class voteController {
 
-	   Connection con; // 멤버변수
-	   Statement stmt;
-	   ResultSet rs;
-
-	   void getConnection() {
-	      String url = "jdbc:oracle:thin:@localhost:1521:xe";
-	      String userid = "root"; //c##추가
-	      String pwd = "1234"; //c##추가
-	      
-	      try 
-	      {
-	         Class.forName("oracle.jdbc.driver.OracleDriver");
-	         System.out.println("드라이버 로드 성공");
-	      }
-	      catch (ClassNotFoundException e) 
-	      {
-	         e.printStackTrace();
-	      }
-	      
-	      try 
-	      {
-	         System.out.println("데이터베이스 연결 준비 .....");
-	         con = DriverManager.getConnection(url,userid,pwd);
-	         System.out.println("데이터베이스 연결 성공");
-	      }
-	      catch (SQLException e) 
-	      {
-	         e.printStackTrace();
-	      }
-	   }
-
-	   void selectNum()
-	   { 
-	      String query = "SELECT * FROM voteList WHERE selectNum = null";
-	      try 
-	      {
-	         stmt = con.createStatement(); //2
-	         rs = stmt.executeQuery(query); //3
-	         System.out.println("CUSTID \tNAME \tADDRESS \tPHONE");
-	      
-	         con.close();
-	      }
-	      catch (SQLException e) 
-	      {
-	         e.printStackTrace();
-	      }
-	   }
+	@RequestMapping("/voteResult")
+	String doubleCheck(voteController votecontroller,Model model) {
+		
+		
+		
+		return "";
+		
+	}
+	   
 }
